06c96bc2ac610f85c8759e387cff2079726cf5a0
AbhishekGL <abhi.globalistic@gmail.com>
2018-09-11 23:54:32 +0200

    First 12 images

---
+++ assets/js/imagebuilder.js
@@ -0,0 +1,17 @@
+ function loadImages(){
+
+  var file = new file('data/images.txt');
+
+  var reader = new FileReader();
+  reader.onload = function(progressEvent){
+    // Entire file
+    console.log(this.result);
+
+    // By lines
+    var lines = this.result.split('\n');
+    for(var line = 0; line < lines.length; line++){
+      console.log(lines[line]);
+    }
+  };
+  reader.readAsText(file);
+};
